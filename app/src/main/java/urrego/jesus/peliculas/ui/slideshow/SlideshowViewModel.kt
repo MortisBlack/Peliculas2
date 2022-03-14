0e7c0b5e769f30a022a55558ba4d0cecdc2ed1dc
MortisBlack <jesus.urrego.iuc@gmail.com>
2022-03-14 14:58:50 -0700

    añadido el chistesito

--- app/src/main/java/urrego/jesus/peliculas/ui/slideshow/SlideshowViewModel.kt
+++ app/src/main/java/urrego/jesus/peliculas/ui/slideshow/SlideshowViewModel.kt
@@ -7,7 +7,7 @@ import androidx.lifecycle.ViewModel
 class SlideshowViewModel : ViewModel() {
 
     private val _text = MutableLiveData<String>().apply {
-        value = "This is slideshow Fragment"
+        value = "Aquí irian mis favortios, si lo hubiera hecho... :c"
     }
     val text: LiveData<String> = _text
 }
